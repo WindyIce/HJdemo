7b6ab5983bca2f1ccd2beaa72daeeb34ffea2aaf
Peiwei Chang <windyicec@gmail.com>
2018-12-06 17:34:56 +0800

    Map

--- Assets/Scripts/SceneGenerator.cs
+++ Assets/Scripts/SceneGenerator.cs
@@ -12,42 +12,81 @@ public class SceneGenerator : BaseSingleton<SceneGenerator> {
     [Header("左上角的点的位置,拖个东西进来")]
     public Transform originPoint;
     [Header("每个格子的长度")]
-    public float eachLength = 5f;
+    public float eachLength = 2f;
     [Header("每个格子的高度")]
-    public float eachHeight = 5f;
+    public float eachHeight = 2f;
+    [Header("从地址读取地图（是），从内容直接读取地图(否)")]
+    public bool readMapFromPath = true;
     [Header("地图文件的位置（相对）")]
     public string configFilePath = "";
+    [Header("地图文件内容字符串")]
+    public string mapContent = "";
     [Header("跟地图文件对应的数字一样的，把prefab拖进来")]
     public GameObject[] prefabIndexCorrespond; // prefab的序号对应的prefab
+    /*
+     * 0_space, 1_collection, 2_wall, 3_oil pollution, 4-6_plastic lv1-3, 7_entrance, 8_exit
+     */
 
-    public Vector3 rightVec = new Vector3(1, 0, 0);
-    public Vector3 downVec = new Vector3(0, 0, 1);
+    [Header("是否通过程序来创建地板，如果否的话，就需要自己创建")]
+    public bool createFloorByProgram = true;
+
+    public Vector3 rightVec = new Vector3(1, 0, 0); // Length
+    public Vector3 downVec = new Vector3(0, 0, -1); // Height
 
     public void Init()
     {
         try
         {
-            string content = ConfigParser.GetFileContent(configFilePath);
+            string content = string.Empty;
+            if (readMapFromPath)
+                content = ConfigParser.GetFileContent(configFilePath);
+            else
+                content = mapContent;
             int[,] map = ConfigParser.GetIntArrayFromString(content, mapLength, mapHeight);
-            for (int i = 0; i < mapLength; i++)
+            if (createFloorByProgram) SpaceCreate();
+            for (int i = 0; i < mapHeight; i++)
             {
-                for (int j = 0; j < mapHeight; j++)
+                for (int j = 0; j < mapLength; j++)
                 {
+                    if (map[i, j] == 0) continue;
+                    else if (map[i, j] == 3) continue;
                     GameObject obj = Instantiate(prefabIndexCorrespond[map[i, j]]);
-                    obj.transform.position = originPoint.position + rightVec * i + downVec * j;
+                    obj.transform.position = originPoint.position
+                        + eachLength * rightVec * (j + 0.5f) + eachHeight * downVec * (i + 0.5f);
+                    if (map[i, j] == 2)
+                    {
+                        obj.transform.localScale = new Vector3(eachLength, 3.0f, eachHeight);
+                    }
                 }
             }
         }
         catch(System.Exception e)
         {
-            print(e);
+            print("地图文件读取错误: "+e);
         }
 
     }
 
+    public void SpaceCreate()
+    {
+        if (originPoint == null)
+        {
+            GameObject originObject = new GameObject("origin");
+            originPoint = originObject.transform;
+            originPoint.position = Vector3.zero;
+        }
+        GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        floor.name = "floor";
+        floor.transform.localScale = new Vector3(mapLength * eachLength, 1, mapHeight * eachHeight);
+        floor.transform.position = new Vector3(
+            originPoint.transform.position.x + mapLength * eachLength / 2,
+            -0.5f,
+            originPoint.transform.position.z - mapHeight * eachLength / 2);
+    }
+
 	// Use this for initialization
 	void Start () {
-
+        Init();
 	}
 	
 	// Update is called once per frame
@@ -61,11 +100,12 @@ public class ConfigParser
     public static int[,] GetIntArrayFromString(string content,int mapLength,int mapHeight)
     {
         string[] lines = content.Split('\n');
-        int[,] toReturn = new int[mapLength, mapHeight];
-        for(int i = 0; i < mapLength; i++)
+        int[,] toReturn = new int[mapHeight, mapLength];
+        for(int i = 0; i < mapHeight; i++)
         {
+            Debug.Log(i);
             string[] nums = lines[i].Split(new char[2] { ' ', '\r' });
-            for(int j = 0; j < mapHeight; j++)
+            for(int j = 0; j < mapLength; j++)
             {
                 char key = char.Parse(nums[j]);
                 int index = (int)char.GetNumericValue(key);
@@ -75,7 +115,7 @@ public class ConfigParser
         return toReturn;
     }
 
-    public static string GetFileContent(string path, bool asyn = false)
+    public static string GetFileContent(string path, bool asyn = true)
     {
         if (asyn)
         {
