a3605f056a5b4bd902fe1ec1d46b2d1e5925b952
Peiwei Chang <windyicec@gmail.com>
2018-12-16 00:35:49 +0800

    New AI Wandering Type
    Not applied yet

---
+++ Assets/Scripts/AgentAI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AgentAI : MonoBehaviour {
+
+    [Header("始终会在一个圆圈之内行动吗")]
+    public bool fixedCircle = true;
+
+    [Header("寻路的半径")]
+    public float radius = 3.0f;
+
+    [Header("每次行程花的时间")]
+    public float wanderTimer = 2.0f;
+
+    private NavMeshAgent navMeshAgent;
+
+    private Vector3 startPosition;
+
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        startPosition = transform.position;
+        timer = wanderTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = MathCore.RandomNavSphere(startPosition, radius, -1);
+            try
+            {
+                if (navMeshAgent == null || navMeshAgent.enabled == false) return;
+                else navMeshAgent.SetDestination(newPos);
+            }
+            catch (System.Exception e) { };
+            timer = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/MathCore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.AI;
+
+class MathCore
+{
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask = -1)
+    {
+        Vector3 randDirection = UnityEngine.Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}
